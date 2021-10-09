0e3be0be2fd971679bcab6c50089d6701705a4a1
Michele Zanda <michele.zanda@gmail.com>
2021-10-09 16:59:45 +0200

    Chrome works as expected. FF has issues in accessing the console logs from WebDriver. Still missing: dockerization

--- hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
+++ hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -1,27 +1,26 @@
 package hellocucumber;
 
 import org.openqa.selenium.JavascriptExecutor;
+import org.junit.Assert;
 
 import io.cucumber.java.After;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.logging.LogEntries;
-import org.openqa.selenium.logging.LogEntry;
 import org.openqa.selenium.logging.LogType;
-import org.openqa.selenium.logging.LoggingPreferences;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.Assert;
+
 import java.time.Duration;
 import java.util.*;
 import java.util.logging.Level;
 import java.net.URL;
-import java.nio.charset.MalformedInputException;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -32,60 +31,77 @@ import io.cucumber.java.en.When;
 
 public class StepDefinitions {
 
-    // DesiredCapabilities caps = DesiredCapabilities.firefox();
-    // LoggingPreferences logPrefs = new LoggingPreferences();
-    // logPrefs.enable(LogType.BROWSER, Level.All);
-    // caps.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);
-    
-    private final WebDriver driver = new FirefoxDriver();
+    private WebDriver driver;
     private int statusCode;
+    private String pageURL;
+
+    @Given("I opened Chrome")
+    public void use_Chrome() {
+        ChromeOptions chOpts = new ChromeOptions().setHeadless(true);
+        driver = new ChromeDriver(chOpts);
+    }
+
+    @Given("I opened Firefox")
+    public void use_Firefox() {
+        FirefoxBinary firefoxBinary = new FirefoxBinary();
+        firefoxBinary.addCommandLineOptions("--headless");
+        
+        FirefoxOptions ffOpts = new FirefoxOptions()
+                    .addPreference("devtools.console.stdout.content", true)
+                    .setHeadless(true)
+                    .setBinary(firefoxBinary);
+
+        driver = new FirefoxDriver(ffOpts);
+    }
     
-    @Given("I am on the W3 bad page")
+    @When("I am on the W3 bad page")
     public void I_visit_bad() {
+        pageURL = "https://www.w3.org/standards/badpage";
         driver.get("https://www.w3.org/standards/badpage");
-    }
-
-    @Given("I am on the W3 multimodal page")
-    public void I_visit_multimodal(){
-        driver.get("https://www.w3.org/standards/webofdevices/multimodal");
-    }
-
-    @Given("I am on the W3 htmlcss page")
-    public void I_visit_htmlcss(){
-        driver.get("https://www.w3.org/standards/webdesign/htmlcss");
-    }
-
-    @When("Page loaded")
-    public void pageReady(String query) {
         this.waitForPageLoaded();
-   }
+    }
 
-   @Then("the console log has no errors")
+    @When("I am on the W3 multimodal page")
+    public void I_visit_multimodal(){
+        pageURL = "https://www.w3.org/standards/webofdevices/multimodal";
+        driver.get("https://www.w3.org/standards/webofdevices/multimodal");
+        this.waitForPageLoaded();
+    }
+
+    @When("I am on the W3 htmlcss page")
+    public void I_visit_htmlcss(){
+        this.pageURL = "https://www.w3.org/standards/webdesign/htmlcss";
+        driver.get("https://www.w3.org/standards/webdesign/htmlcss");
+        this.waitForPageLoaded();
+    }
+
+   @Then("The console log has no errors")
    public void checkNoErrorsInConsole() {
         LogEntries logs = driver.manage().logs().get(LogType.BROWSER);
         logs.forEach((log) -> {
             if (log.getLevel() == Level.SEVERE || log.getLevel() == Level.SEVERE) {
-                Assert.fail("Console has errors");
+                Assert.fail("Message in console:" + log.getMessage());
             }
         });
    }
 
    @Then("response code")
-   public void checkResponseCode(String pageURL) {
+   public void checkResponseCode() {
         try {
-            statusCode= this.getResponseCode(pageURL);
+            statusCode= this.getResponseCode(this.pageURL);
         } catch (Throwable error) {
-            Assert.fail("Unable to get status code");
+            Assert.fail(error.getMessage());
         }
         
-        if(statusCode >= 400 || statusCode < 500){
+        if(statusCode >= 400 && statusCode < 500){
             // Broken link found -- Test failed
-            Assert.fail("Broken links found in page");
+            Assert.fail("Status code returned: " + statusCode);
         }
+        Assert.assertEquals(200, statusCode);;
    }
 
    @Then("Page has no broken links")
-   public void checkNoBrokenLinks(String titleStartsWith) {
+   public void checkNoBrokenLinks() {
         List<WebElement> links = driver.findElements(By.tagName("a"));
         for(int i = 0; i < links.size(); i++){
             if(!(links.get(i).getAttribute("href") == null) && !(links.get(i).getAttribute("href").equals(""))){
@@ -93,12 +109,12 @@ public class StepDefinitions {
                     try {
                         statusCode= this.getResponseCode(links.get(i).getAttribute("href").trim());
                     } catch (Throwable error) {
-                        Assert.fail("Unable to get status code");
+                        Assert.fail("Exception:" + error.getMessage());
                     }
                     
-                    if(statusCode >= 400 || statusCode < 500){
+                    if(statusCode >= 400 && statusCode < 500){
                         // Broken link found -- Test failed
-                        Assert.fail("Broken links found in page");
+                        Assert.fail("Status code returned: " + statusCode);
                     }
                 }
             }   
@@ -118,7 +134,7 @@ public class StepDefinitions {
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
             wait.until(expectation);
         } catch (Throwable error) {
-            Assert.fail("Timeout waiting for Page Load Request to complete.");
+            Assert.fail("Exception:" + error.getMessage());
         }
     }
 
---
+++ hellocucumber/src/test/resources/hellocucumber/no_console_errors.feature
@@ -0,0 +1,32 @@
+Feature: No console errors?
+    Check no errors are shown in console
+
+    Scenario: Check W3 bad page loading raises no errors in console
+        Given I opened Chrome
+        When I am on the W3 bad page
+        Then The console log has no errors
+
+    Scenario: Check W3 multimodal page loading raises no errors in console
+        Given I opened Chrome
+        When I am on the W3 multimodal page
+        Then The console log has no errors
+
+    Scenario: Check W3 htmlcss page loading raises no errors in console
+        Given I opened Chrome
+        When I am on the W3 htmlcss page
+        Then The console log has no errors
+
+        Scenario: Check W3 bad page loading raises no errors in console
+        Given I opened Firefox
+        When I am on the W3 bad page
+        Then The console log has no errors
+
+    Scenario: Check W3 multimodal page loading raises no errors in console
+        Given I opened Firefox
+        When I am on the W3 multimodal page
+        Then The console log has no errors
+
+    Scenario: Check W3 htmlcss page loading raises no errors in console
+        Given I opened Firefox
+        When I am on the W3 htmlcss page
+        Then The console log has no errors
